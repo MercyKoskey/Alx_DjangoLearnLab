d19ceeb1930dc9a9330f6fa9144e5a0c165a6741
Mercy Koskey <koskey.mercy@gmail.com>
2025-07-18 22:32:01 +0300

    creating a booklist and a library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
+# Function-based view for listing all books
 def list_books(request):
-    books = Book.objects.all()  # get all books from the database
+    books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Class-based view for showing a specific library and its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = Book.objects.filter(library=self.object)
+        return context
