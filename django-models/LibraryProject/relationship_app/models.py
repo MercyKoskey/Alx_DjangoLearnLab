03bfe222d9906810b8ee292c100b1e854f1959dd
Mercy Koskey <koskey.mercy@gmail.com>
2025-07-22 14:29:59 +0300

    updates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,6 +6,21 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -20,6 +35,7 @@ class Book(models.Model):
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book"),
         ]
+
 
 
 
