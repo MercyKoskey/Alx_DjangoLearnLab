707af0d1c7a89b35bbcac2afbebfe250be506ca4
Mercy Koskey <koskey.mercy@gmail.com>
2025-07-28 01:07:59 +0300

    changes

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,9 +1,9 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# class CustomUser(AbstractUser):
-#     date_of_birth = models.DateField()
-#     profile_photo = models.ImageField()
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
 
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
