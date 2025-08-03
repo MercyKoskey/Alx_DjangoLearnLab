2ed786fc81a03da1a03f04b3c6f55eff9f7f66dc
Mercy Koskey <koskey.mercy@gmail.com>
2025-08-03 09:08:31 +0300

    Developing  a simple API endpoint using Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,9 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
+    
