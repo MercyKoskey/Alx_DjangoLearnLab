26062eec7b5076fed7c8f9adf9a98dd17e04f8b5
Mercy Koskey <koskey.mercy@gmail.com>
2025-08-16 19:12:27 +0300

    setting up a blog project

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm, UserUpdateForm
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after register
+            messages.success(request, "Registration successful!")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome back {username}!")
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = LoginForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('login')
+
+@login_required
+def edit_profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully!")
+            return redirect('home')
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'blog/edit_profile.html', {'form': form})
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
+
+
+def home_view(request):
+    return render(request, "blog/base.html") 
+
+def post_view(request):
+    return render(request, "blog/posts.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('', post_view, name='posts'),
+    path('', include('blog.urls')),
 ]
