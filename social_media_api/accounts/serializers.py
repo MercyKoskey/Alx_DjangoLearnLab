2f125e1ab58934d48f1ad0bd462ced9d06137a49
Mercy Koskey <koskey.mercy@gmail.com>
2025-08-21 17:17:15 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
@@ -11,18 +11,24 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
+
 class LoginSerializer(serializers.Serializer):
-    identifier = serializers.CharField(required=True)  # Can be username or email
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    identifier = serializers.CharField(required=True)  # username or email
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
 
     def validate(self, attrs):
         identifier = attrs.get('identifier')
@@ -48,4 +54,3 @@ class LoginSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
-
